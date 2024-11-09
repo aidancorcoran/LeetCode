007bed6d217d3c6135e14662fccb22ffb97fb064
aidancorcoran <ajcorcoran2001@gmail.com>
2024-11-09 12:59:34 -0500

    incomplete solution

--- easy/67_add_binary/solution.py
+++ easy/67_add_binary/solution.py
@@ -18,12 +18,18 @@
 # Each string does not contain leading zeros except for the zero itself.
 # -------------------------------------------------------------------------------------------
 # First Thoughts:
-# Can add as string or convert to ints and then return a string
+# Can add as string or convert to ints (this would use built in functions) and then return a string
 # Potentially loop from the last index backwards if adding as a string
+
+# We need to check each number backwards, if they are both 1's the result at that index is 0 and carry is 1
+# Can build a string and then reverse it
 
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        a_int = int(a, base=2)
-        b_int = int(b, base=2)
-
-        return(bin(a_int+b_int)[2:])
+        a_index = len(a) - 1
+        b_index = len(b) - 1
+        carry = 0
+        sum = []
+        while a_index >= 0 or b_index >= 0:
+            if a[a_index] == 1 and b[b_index] == 1 and carry == 1:
+                
