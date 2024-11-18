8af34485f5b1ee1027b50b203deab8e162d8ab10
aidancorcoran <ajcorcoran2001@gmail.com>
2024-11-17 21:00:11 -0500

    initial setup for problem 83

---
+++ easy/83_remove_duplicates_sorted_list/test.py
@@ -0,0 +1,26 @@
+from solution import Solution, ListNode
+
+solutionObj = Solution()
+
+node1 = ListNode(1)
+node2 = ListNode(1)
+node3 = ListNode(2)
+node4 = ListNode(3)
+node5 = ListNode(3)
+node1.next = node2 
+node2.next = node3
+node3.next = node4
+node4.next = node5
+
+
+
+
+
+
+
+
+
+
+def printLinkedList(list_head):
+    while list.next != None:
+        print(no)
